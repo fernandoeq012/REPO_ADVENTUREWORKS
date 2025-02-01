b699e5f664e03941cab242d2f1630058a957648a
Fernando Moreira <flmoreira3@gmail.com>
2025-02-01 17:52:33 +0000

    Finalizada stg clientes

---
+++ models/staging/crm/stg_crm__customer.sql
@@ -0,0 +1,21 @@
+with
+    fonte_customer as (
+        select
+        CUSTOMERID
+        , PERSONID
+        , STOREID
+        , TERRITORYID
+        from {{ source('crm_adventureworks', 'CUSTOMER') }}
+    )
+
+    , renomeacao as (
+        select
+            cast(CUSTOMERID as int) as pk_cliente
+            , cast(PERSONID as int) as fk_pessoa
+            , cast(STOREID as int) as fk_loja
+            , cast(TERRITORYID as int) as fk_territorio
+        from fonte_customer
+    )
+
+select *
+from renomeacao
